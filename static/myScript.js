28ce46e7da7c3218ff52504b811dd67ac96ae849
Matthew <60951840+MatthewKaiHin@users.noreply.github.com>
2023-02-26 03:44:34 +0800

    Add files via upload

--- static/myScript.js
+++ static/myScript.js
@@ -2,44 +2,52 @@
 function display_content(page) {
     var about_content = document.getElementById("about_section");
     var resume_content = document.getElementById("resume_section");
+    var skills_content = document.getElementById("skills_section");
     var contact_content = document.getElementById("contact_section");
 
     if (page === "about") {
         about_content.style.display = "block";
         resume_content.style.display = "none";
+        skills_content.style.display = "none";
         contact_content.style.display = "none";
     } else if (page === "resume") {
         about_content.style.display = "none"
         resume_content.style.display = "block";
+        skills_content.style.display = "none";
+        contact_content.style.display = "none";
+    } else if (page === "skills") {
+        about_content.style.display = "none"
+        resume_content.style.display = "none";
+        skills_content.style.display = "block";
         contact_content.style.display = "none";
     } else if (page === "contact") {
         about_content.style.display = "none"
         resume_content.style.display = "none";
+        skills_content.style.display = "none";
         contact_content.style.display = "block";
     };
 };
 /* section display controller end */
 
-/* display alert box start */
-function alert_modal() {
-    var modal = document.getElementById("about_modal");
-    modal.style.display = "block";
-
-    document.getElementById("close").addEventListener("click", function(){
-        modal.style.display = "none";
-    });
-};
-/* display alert box end */
-
 /* audio auto play start */
+function click_me() {
+    var click_me = document.getElementById("click_me");
+    var about_img = document.getElementById("about_img");
+
+    click_me.style.display = "none";
+    about_img.style.display = "block";
+}
+
 function audio_play() {
     var audio = document.getElementById("about_audio");
+
     audio.muted = false;
     audio.play();
 };
 
 function audio_pause() {
     var audio = document.getElementById("about_audio");
+
     audio.muted = true;
     audio.pause();
 };
